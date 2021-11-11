25821bff467f901e757ee15647fac180e72a44fc
kennie <245794335@qq.com>
2021-11-11 09:06:55 +0800

    解决 Android P 禁用 http 请求的问题

---
+++ base_project/src/main/java/com/kennie/base/project/BaseFileProvider.java
@@ -0,0 +1,9 @@
+package com.kennie.base.project;
+
+import androidx.core.content.FileProvider;
+
+/**
+ * 自定义FileProvider 解决FileProvider冲突问题
+ */
+public class BaseFileProvider extends FileProvider {
+}
